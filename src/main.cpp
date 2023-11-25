48e162d5db55b6a8b57862db6cb8a5d34414600d
Rick-02 <ricardo@fffuego.com>
2023-11-24 23:09:57 -0600

    add funcionality for ultrasonic and IR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,64 @@
 #include <Arduino.h>
 
-void setup() {
-  // Comunicación seria a 9600 baudios
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-}
+// Define los pines para el sensor HC-SR04
+const int triggerPin = 9;
+const int echoPin = 10;
+const int analogPin = A0;
 
+// Tipo de sensor: "ULTRASONICO" o "INFRARROJO"
+#define SENSOR_TYPE "ULTRASONICO"
+
+// Prototipos de las funciones
+float leerDistanciaInfrarrojo(int n);
+float leerDistanciaUltrasonico();
+
+void setup() {
+  Serial.begin(115200);
+
+  if (String(SENSOR_TYPE) == "ULTRASONICO") {
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+  }
+}
 
 void loop() {
-  
-  long tiempo=millis(); //tiempo antes de iniciar la lectura
-  int D_cm=distancia(20); //lectura de distancia
-  tiempo=millis()-tiempo; //milisegundos que duró la lectura
-  Serial.print("Tiempo de lectura: ");
-  Serial.print(tiempo); 
-  Serial.print("ms  Distancia: ");
-  Serial.print(D_cm);
-  Serial.println("  cm");
-  delay(100);
+  float distancia;
+
+  if (String(SENSOR_TYPE) == "ULTRASONICO") {
+    distancia = leerDistanciaUltrasonico();
+  } else if (String(SENSOR_TYPE) == "INFRARROJO") {
+    distancia = leerDistanciaInfrarrojo(20);
+  }
+
+  if (distancia < 60) {
+
+    // Envía la distancia al Serial Plotter
+    Serial.print("0 ");
+    Serial.print("60 ");
+    Serial.println(distancia);
+  }
+
+  delay(10); // Espera un segundo para la siguiente medición
 }
 
-float distancia(int n)
-{
-  long suma=0;
-  for(int i=0;i<n;i++)
-  {
-    suma=suma+analogRead(A0);
+float leerDistanciaInfrarrojo(int n) {
+  long suma = 0;
+  for (int i = 0; i < n; i++) {
+    suma += analogRead(analogPin);
   }  
-  float adc=suma/n;
-  float distancia_cm = 17569.7 * pow(adc, -1.2062);
-  return(distancia_cm);
+  float adc = suma / n;
+  float distancia_cm = 9462.7 * pow(adc, -1.115) + 3.297;
+
+  return constrain(distancia_cm, 5.0, 80.0);
+}
+
+float leerDistanciaUltrasonico() {
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  
+  long duration = pulseIn(echoPin, HIGH);
+  return duration * 0.034 / 2;
 }
